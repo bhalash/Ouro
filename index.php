111b45c695701106270f6ba5be9ba408cd834d28
Mark <mark@bhalash.com>
2015-03-19 22:50:49 +0000

    Renamed index file for PHP functionality

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <!-- Whether a thought is spoken or not it is a real thing with powers of reality. -->
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="no">
+    <?php $title = (strpos($_SERVER['HTTP_HOST'], 'bhalash') !== false) ? 'Ix Sandbox' : 'Ouro'; ?>
+    <title><?php printf($title); ?></title>
+    <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro|Fjalla+One|Source+Code+Pro|Open+Sans+Condensed:300" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="assets/sass/style.css" />
+    <!-- [if IE]><link rel="shortcut icon" href="images/favicon.ico"><![endif] -->
+    <link rel="icon" href="images/favicon.png">
+    <!-- Bing Webmaster Tools -->
+    <meta name="msvalidate.01" content="205E68696E2A0E9A5E633C8D8FEA45CA" />
+    <!-- Open Graph -->
+    <meta property="og:site_name" content="Ouro-botics">
+    <meta property="og:title" content="Ouro-botics">
+    <meta property="og:url" content="<?php printf('%s%s', 'http://', $_SERVER['HTTP_HOST']); ?>">
+    <meta property="og:description" content="Bioprinting-tissue engineering-is the next step: skin grafts, bones and eventually whole replacement organs will be grown on demand from scratch. Ouro-botics plan to be at the forefront of this field.">
+    <meta property="og:image" content="<?php printf('%s%s%s', 'http://', $_SERVER['HTTP_HOST'], '/ouro.jpg'); ?>">
+    <meta property="og:image:width" content="1920">
+    <meta property="og:image:height" content="1080">
+    <meta property="og:type" content="website">
+    <!-- en_IE exists, but some Open Graph scrapers *coughfacebookandgooglecough* don't recognize it. -->
+    <meta property="og:locale" content="en_GB">
+    <!-- Twitter Cards -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@ouro_botics">
+    <meta name="twitter:title" content="Ouro">
+    <meta name="twitter:description" content="Bioprinting-tissue engineering-is the next step: skin grafts, bones and eventually whole replacement organs will be grown on demand from scratch. Ouro plans to be at the forefront of this field.">
+    <meta name="twitter:url" content="<?php printf('%s%s', 'http://', $_SERVER['HTTP_HOST']); ?>">
+</head>
+    <body>
+        <a class="twitter" id="ouro-twitter" rel="nofollow" target="_blank" title="@ouro_botics" href="https://www.twitter.com/ouro_botics"></a>
+        <section id="home">
+            <div class="logo">
+                <h1>ouro</h1>
+            </div>
+        </section>
+        <nav id="menu">
+            <ul>
+                <li><a class="current-nav nav" href="#home">Home</a></li>
+                <li><a class="nav" href="#intro">Bioprinting</a></li>
+                <li><a class="nav" href="#ouro">The Ouro Bioprinter</a></li>
+                <li><a class="nav" href="#team">The Ouro Team</a></li>
+                <li><a class="nav" href="#contact">Contact Us</a></li>
+            </ul>
+        </nav>
+        <div id="content">
+            <section id="intro">
+                <div class="side">
+                    <div class="wrapper">
+                        <h3 class="sec-title">Bioprinting</h3>
+                        <p>
+                        3D printing is, today, one of the most exciting and dynamic fields of manufacturing technologies. 3D printers reduce the complexity and turnaround time of the prototyping workflow: dream it, draw it, print it and repeat until you have the perfect design.
+                        </p>
+                        <p>An amazing range of materials has been used to 3D print: plastics, chocolate, ice, water, metal, paper and even modelling clay.</p>
+                        <p>Bioprinting-tissue engineering-is the next step: skin grafts, bones and eventually whole replacement organs will be grown on demand from scratch.</p>
+                        <p>Ouro-botics plan to be at the forefront of this field.</p>
+                    </div>
+                </div>
+                <div class="main">
+                    <div class="wrapper">
+                    </div>
+                </div>
+            </section>
+            <section id="ouro">
+                <div class="side">
+                    <div class="wrapper">
+                        <h3>The Ouro prototype</h3>
+                        <p>
+                            The Ouro bioprinter will be coming to the market in 2015.
+                        </p>
+                    </div>
+                </div>
+                <div class="main">
+                    <div class="wrapper">
+                        <div class="gallery" id="gallery">
+                            <div class="image-col">
+                                <a class="image" href="/assets/images/gallery/full/0.jpg"><img src="/assets/images/gallery/thumb/0.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/1.jpg"><img src="/assets/images/gallery/thumb/1.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/2.jpg"><img src="/assets/images/gallery/thumb/2.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/3.jpg"><img src="/assets/images/gallery/thumb/3.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/4.jpg"><img src="/assets/images/gallery/thumb/4.jpg" alt="Grahhhh" /></a>
+                            </div>
+                            <div class="image-col">
+                                <a class="image" href="/assets/images/gallery/full/5.jpg"><img src="/assets/images/gallery/thumb/5.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/6.jpg"><img src="/assets/images/gallery/thumb/6.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/7.jpg"><img src="/assets/images/gallery/thumb/7.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/8.jpg"><img src="/assets/images/gallery/thumb/8.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/9.jpg"><img src="/assets/images/gallery/thumb/9.jpg" alt="Grahhhh" /></a>
+                            </div>
+                            <div class="image-col">
+                                <a class="image" href="/assets/images/gallery/full/10.jpg"><img src="/assets/images/gallery/thumb/10.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/11.jpg"><img src="/assets/images/gallery/thumb/11.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/12.jpg"><img src="/assets/images/gallery/thumb/12.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/13.jpg"><img src="/assets/images/gallery/thumb/13.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/14.jpg"><img src="/assets/images/gallery/thumb/14.jpg" alt="Grahhhh" /></a>
+                            </div>
+                            <div class="image-col">
+                                <a class="image" href="/assets/images/gallery/full/15.jpg"><img src="/assets/images/gallery/thumb/15.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/16.jpg"><img src="/assets/images/gallery/thumb/16.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/17.jpg"><img src="/assets/images/gallery/thumb/17.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/18.jpg"><img src="/assets/images/gallery/thumb/18.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/19.jpg"><img src="/assets/images/gallery/thumb/19.jpg" alt="Grahhhh" /></a>
+                            </div>
+                            <div class="image-col">
+                                <a class="image" href="/assets/images/gallery/full/20.jpg"><img src="/assets/images/gallery/thumb/20.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/21.jpg"><img src="/assets/images/gallery/thumb/21.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/22.jpg"><img src="/assets/images/gallery/thumb/22.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/23.jpg"><img src="/assets/images/gallery/thumb/23.jpg" alt="Grahhhh" /></a>
+                                <a class="image" href="/assets/images/gallery/full/24.jpg"><img src="/assets/images/gallery/thumb/24.jpg" alt="Grahhhh" /></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="team">
+                <div class="side">
+                    <div class="wrapper">
+                        <h3 class="sec-title">The Ouro team</h3>
+                        <p>Staffed by rugged beards and fearsome pirate queeens, Ouro's diverse team is drawn from two continents.</p>
+                        <div class="teamlist">
+                            <div class="member" id="jemma">
+                                <a class="avatar" target="_blank" href="javascript:void(0)"></a>
+                                <div class="bio">
+                                    <span class="name">Jemma Redmond</span>
+                                    <span class="title">Ouro Designer and CEO</span>
+                                    <span class="blurb">The found and CEO of Ouro-botics, this University College Dublin alumni provides us vision and guidance in our plan to conquer the world even before her first cup of morning coffee.</span>
+                                    <ul class="social">
+                                        <li class="twitter"><a target="_blank" rel="nofollow" href="https://www.twitter.com/sciencegirlg"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="member" id="alanna">
+                                <a class="avatar" target="_blank" href="javascript:void(0)"></a>
+                                <div class="bio">
+                                    <span class="name">Alanna Kelly</span>
+                                    <span class="title">Hardware Guru</span>
+                                    <span class="blurb">The Ouro's hardware guru, Alanna brings the brawn to match Jemma's brains. Alanna handles the development and assembly of the Ouro printer.</span>
+                                </div>
+                            </div>
+                            <div class="member" id="mark">
+                                <a class="avatar" target="_blank" href="javascript:void(0)"></a>
+                                <div class="bio">
+                                    <span class="name">Mark Grealish</span>
+                                    <span class="title">Web Developer</span>
+                                    <span class="blurb">Tall, handsome, manly: many men have earned at least one of these distinguished adjectives in their lives. Mark has not. In the meantime, this Software Design graduate handles the Ouro website.</span>
+                                    <ul class="social">
+                                        <li class="twitter"><a target="_blank" rel="nofollow" href="https://www.twitter.com/bhalash"></a></li>
+                                        <li class="github"><a target="_blank" rel="nofollow" href="https://www.github.com/bhalash"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="member" id="joe">
+                                <a class="avatar" target="_blank" href="javascript:void(0)"></a>
+                                <div class="bio">
+                                    <span class="name">Joe Keane</span>
+                                    <span class="title">Design Engineer</span>
+                                    <span class="blurb">Joe is a talented mechanical engineer who has contributed greatly to the architecture of the Ouro bioprinter.</span>
+                                    <ul class="social">
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="main">
+                    <div class="wrapper">
+                        <div id="team-feature">                      
+                            <div class="avatar">
+                                <ul class="social"></ul> 
+                            </div>
+                            <p><span class="name"></span><span class="blurb"></span></p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section id="contact">
+                <div class="side">
+                    <div class="wrapper">
+                        <h3 class="sec-title">Would you like to know more?</h3>
+                        <p>Stay in the loop on the latest Ouro developments on our mailing list, or follow <a title="@ouro_botics" href="https://www.twitter.com/ouro_botics">@ouro_botics</a> on Twitter!</p>
+                        <nav id="social">
+                            <ul>
+                                <li class="twitter"><a title="@ouro_botics" href="https://www.twitter.com/ouro_botics"></a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="main">
+                    <div class="wrapper">
+                        <p class="messages">&nbsp;</p>
+                        <form action="/contact.php" autocomplete="off" id="sendmessage" method="post" novalidate>
+                            <p>
+                                <input id="name" name="name" type="text" placeholder="Name" required>
+                                <input id="email" name="email" type="email" placeholder="Email" required>
+                                <textarea id="message" name="message" placeholder="Message" required></textarea>
+                                <input id="unseen" name="unseen" type="hidden"> 
+                                <button id="submit" type="submit">Submit</button>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+                <div id="footer">
+                    <p>Copyright &copy; 2015 <a title="Tr3life home" href="http://tr3life.eu/">Tr3life</a><small>&trade;</small>. All rights reserved.</p>
+                </div>
+            </section>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="assets/js/modernizr_touch.js"></script>
+        <script src="assets/js/browser_detect.js"></script>
+        <script src="assets/js/main.js"></script>
+        <script src="assets/js/nav.js"></script>
+        <script src="assets/js/team.js"></script>
+        <script src="assets/js/gallery.js"></script>
+        <script src="assets/js/contact.js"></script>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-59959595-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+    </body>
+</html>
