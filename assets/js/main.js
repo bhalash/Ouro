11d1722c44eae5fde1a2a3a98d98ef16a3651ff0
Mark <mark@bhalash.com>
2015-03-26 06:57:54 +0000

    Added several functions
    * Moved Google Analytics and MailChimp to this file.
    * Clarified and reduced use of resize function to relevant devices.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,88 @@
 jQuery(document).ready(function($) {
     'use strict';
     /*
-     * Fallback for iOS Devices
+     * Dynamic Section Resizing
      * ------------------------
-     * iOS 6's and 7 have a really fucking awful bug in regards the vh unit. 
-     * This is an ugly fallback that changes the units to % of the viewport
-     * instead of vh of the viewport. 
+     * Neither Internet Explorer nor Safari Mobile have great/any support for 
+     * calc with vh, as I use in _sections.scss:
+     *
+     * @include prefix(min-height, calc(100vh - #{$navigation_menu_height}));
+     * 
+     * This compiles to:
+     *
+     * calc(100vh - 70px);
+     * 
+     * Because their support is incomplete I use this as a fallback.
      */
 
     var sectionSizes = function() {
-        $('section').not('#home').each(function() {
-            // There is currently no support in CSS3 for calc(vh - px);
-            var navHeight = $('nav#menu').height();
+        var $section = $('section').not('#home'),
+            $nav = $('nav#menu');
 
-            if (!$('html').hasClass('internet-explorer')) {
-                $(this).css('min-height', $(window).height() - navHeight);
-            } else {
-                /* I would much rather use min-height for this, but TL;DR IE 11
-                 * will not vertically center children if the parent has a min-height
-                 * attribute set. */
-                $(this).css('height', $(window).height() - navHeight);
-            }
+        $section.css('min-height', $(window).height() - $nav.height());
+    }
+
+    if ($('html').hasClass('ipad') || $('html').hasClass('internet-explorer')) {
+        $(window).on('load', sectionSizes);
+        $(window).on('resize', sectionSizes);
+    }
+
+    $('input').keypress(function(event) {
+        // Submit newsletter form on return.
+        if (event.which == 13) {
+            event.preventDefault();
+            $('form').submit();
+        }
+    });
+
+    /*
+     * Fade-in Selected Elements on Page Scroll
+     * ----------------------------------------
+     */
+
+    var toFadeIn = [
+        '.gallery a', 
+        '.member'
+    ];
+
+    var showOnScroll = function() {
+        $.each(toFadeIn, function(i,v) {
+            $(v).each(function() {
+                if (parseInt($(v).css('opacity')) !== 1) {
+                    var scrollTop = $(window).scrollTop(),
+                        winHeight = $(window).height() * 0.9,
+                        thisTop = $(v).offset().top;
+
+                    if (thisTop < scrollTop + winHeight) {
+                        $(v).animate({ 'opacity' : '1' }, 700);
+                    }
+                }
+
+            });
         });
     }
 
-    $(window).on('load', sectionSizes);
-    $(window).on('resize', sectionSizes);
+    $(window).on('scroll', showOnScroll);
+    $(window).on('load', showOnScroll);
 
-    /*
-     * Miscellaneous
-     * -------------
-     */
 });
+
+/*
+ * Google Analytics
+ * ----------------
+ */
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-59959595-1', 'auto');
+ga('send', 'pageview');
+
+/*
+ * MailChimp
+ * ---------
+ */
+
+(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
