4d91bb1f4e01ae1719f7725c8e4ee909495513dc
Mark <mark@bhalash.com>
2015-03-26 06:45:47 +0000

    Renamed wrapper container

--- includes/sections/section-product.php
+++ includes/sections/section-product.php
@@ -1,24 +1,26 @@
-<section class="squished" id="product">
-    <h3 class="sec-title">The <?php printf(PRODUCT_NAME); ?> prototype</h3>
-    <p>The <?php printf(PRODUCT_NAME); ?> bioprinter will be coming to the market in 2015.</p>
-    <div class="feature gallery" id="gallery">
-        <?php 
-        /* Lazy iteration of images. There are no descriptions
-         * available for images. */
-        $ouro_images = array(
-            'full' => '/assets/images/gallery/full/',
-            'thumb' => '/assets/images/gallery/thumb/',
-            'alt' => 'The ' . PRODUCT_NAME . ' Bioprinter',
-            'extension' => '.jpg'
-        );
-
-        for ($i = 0; $i <= 23; $i++) {
-            printf('<a class="image" href="%s"><img src="%s" alt="%s" /></a>', 
-                $ouro_images['full'] . $i . $ouro_images['extension'], 
-                $ouro_images['thumb'] . $i . $ouro_images['extension'], 
-                $ouro_images['alt'] 
+<section id="product">
+    <div class="content">
+        <h3 class="sec-title">The <?php printf(PRODUCT_NAME); ?> prototype</h3>
+        <p>The <?php printf(PRODUCT_NAME); ?> bioprinter will be coming to the market in 2015.</p>
+        <div class="feature gallery" id="gallery">
+            <?php 
+            /* Lazy iteration of images. There are no descriptions
+             * available for images. */
+            $ouro_images = array(
+                'full' => '/assets/images/gallery/full/',
+                'thumb' => '/assets/images/gallery/thumb/',
+                'alt' => 'The ' . PRODUCT_NAME . ' Bioprinter',
+                'extension' => '.jpg'
             );
-        } 
-        ?>
+
+            for ($i = 0; $i <= 23; $i++) {
+                printf('<a class="image" href="%s"><img src="%s" alt="%s" /></a>', 
+                    $ouro_images['full'] . $i . $ouro_images['extension'], 
+                    $ouro_images['thumb'] . $i . $ouro_images['extension'], 
+                    $ouro_images['alt'] 
+                );
+            } 
+            ?>
+        </div>
     </div>
 </section>
