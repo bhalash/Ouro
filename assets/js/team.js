16831bb600519b53c683e8c6291483b5dc692e2e
Mark <mark@bhalash.com>
2015-03-26 06:57:08 +0000

    Adjusted variables
    I separated blind selectors and elements from team selectors and
    elements for the sake of reusability.

--- assets/js/team.js
+++ assets/js/team.js
@@ -11,38 +11,45 @@ $(document).ready(function($) {
 
     var team = {
         member: '.teamlist div.member',
-        avatar: '.teamlist div.member a.avatar',
-        blind: 'div[class^=blind-]',
-        bio: 'div.bio'
+        avatar: '.teamlist div.member a.avatar'
+    };
+
+    var blinds = {
+        selector: 'div[class^=blind-]',
+        top: '<div class="blind-top">',
+        bottom: '<div class="blind-bottom">',
+        description: 'div.description',
+        parent: '.has-blinds'
     };
 
     $(team.avatar).each(function() {
-        $(this).prepend('<div class="blind-top">').prepend('<div class="blind-bottom">');
+        $(this).prepend(blinds.top).prepend(blinds.bottom);
+        $(this).parent().addClass(blinds.parent.substring(1));
     });
 
     $.fn.extend({
         showBio: function() {
-            $(this).find(team.blind).css('height', '50%');
-            $(this).find(team.bio).css('opacity', 1);
+            $(this).find(blinds.selector).css('height', '50%');
+            $(this).find(blinds.description).css('opacity', 1);
         },
         hideBio: function() {
-            $(this).find(team.blind).css('height', 0);
-            $(this).find(team.bio).css('opacity', 0);
+            $(this).find(blinds.selector).css('height', 0);
+            $(this).find(blinds.description).css('opacity', 0);
         }
     });
 
     if ($('html').hasClass('touch')) {
         $(window).on('swipe', function() {
-            $(team.member).hideBio();
+            $(blinds.parent).hideBio();
         });
 
-        $(team.member).on('tap', function(event) {
-            $(team.member).not(this).hideBio();
+        $(blinds.parent).on('tap', function(event) {
+            $(blinds.parent).not(this).hideBio();
             $(this).showBio();
             event.preventDefault();
         });
     } else {
-        $(team.member).hover(function() {
+        $(blinds.parent).hover(function() {
             $(this).showBio();
         }).mouseleave(function() {
             $(this).hideBio();
