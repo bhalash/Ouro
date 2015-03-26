9636f9052bdae3c1f0baef9757d18104ca736557
Mark <mark@bhalash.com>
2015-03-26 06:47:59 +0000

    Replaced contact form and changed interior class

--- includes/sections/section-contact.php
+++ includes/sections/section-contact.php
@@ -1,18 +1,24 @@
 <section class="inverse" id="contact">
-    <h3 class="sec-title">Would you like to know more?</h3>
-    <p>Stay in the loop on the latest Ouro developments on our mailing list, check out <a href="<?php printf(COMPANY_BLOG); ?>">our blog</a>, or follow <a title="<?php printf('@%s', COMPANY_TWITTER); ?>" href="https://www.twitter.com/<?php printf('%s', COMPANY_TWITTER); ?>"><?php printf('@%s', COMPANY_TWITTER); ?></a> on Twitter!</p>
-    <p class="messages">&nbsp;</p>
-    <form action="/contact.php" autocomplete="off" id="sendmessage" method="post" novalidate>
-        <p>
-            <span class="form-inputs">
-                <input id="name" name="name" type="text" placeholder="Name" required>
-                <input id="email" name="email" type="email" placeholder="Email" required>
-            </span>
-            <textarea id="message" name="message" placeholder="Message" required></textarea>
-            <input id="unseen" name="unseen" type="hidden">
-            <button id="submit" type="submit">Submit</button>
-        </p>
-    </form>
+    <div class="content">
+        <h3 class="sec-title">Would you like to know more?</h3>
+        <p>Stay in the loop on the latest Ouro developments on our mailing list, email <a href="mailto:<?php printf(COMPANY_GREETER_EMAIL); ?>"><?php printf(COMPANY_GREETER_EMAIL); ?></a>, check out <a href="<?php printf(COMPANY_BLOG); ?>">our blog</a>, or follow <a title="<?php printf('@%s', COMPANY_TWITTER); ?>" href="https://www.twitter.com/<?php printf('%s', COMPANY_TWITTER); ?>"><?php printf('@%s', COMPANY_TWITTER); ?></a> on Twitter!</p>
+
+        <div id="mc_embed_signup">
+            <form action="//ouro.us10.list-manage.com/subscribe/post?u=d09d427a327a7172115624240&amp;id=c48802699f" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                    <div style="position: absolute; left: -5000px;">
+                        <input type="text" name="b_d09d427a327a7172115624240_c48802699f" tabindex="-1" value="">
+                    </div>
+                    <div class="clear">
+                        <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
     <div id="footer">
         <p>Copyright &copy; 2015 <a title="Tr3life home" href="http://tr3life.eu/">Tr3life</a><small>&trade;</small>. All rights reserved.</p>
     </div>
