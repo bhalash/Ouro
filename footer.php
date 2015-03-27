6ab82ab18481fc0b8ab8f0d9e66ac21c29c2c4ca
Mark <mark@bhalash.com>
2015-03-27 07:49:30 +0000

    Added ignore for jQuery Mobile

--- footer.php
+++ footer.php
@@ -2,6 +2,12 @@
         <?php // modernizr needs to load first or it will cause an erronous detection of touch support ?>
         <script src="<?php printf('%s', ASSETS . '/js/modernizr_touch.js'); ?>"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script>
+            $(document).bind("mobileinit", function(){
+                $.mobile.ignoreContentEnabled = true;
+            });
+        </script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
         <script src="<?php printf('%s', ASSETS . '/js/browser_detect.js'); ?>"></script>
         <script src="<?php printf('%s', ASSETS . '/js/main.js'); ?>"></script>
         <script src="<?php printf('%s', ASSETS . '/js/nav.js'); ?>"></script>
