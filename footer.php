b3085d0edb26d66ba7f1600136a433253ac1c9b6
Mark <mark@bhalash.com>
2015-03-31 22:24:45 +0100

    Minified JavaScript

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
         </div> <?php // End #content ?>
         <?php // modernizr needs to load first or it will cause an erronous detection of touch support ?>
-        <script src="<?php printf('%s', ASSETS . '/js/modernizr_touch.js'); ?>"></script>
+        <script src="<?php printf('%s', ASSETS . '/js/modernizr_touch.min.js'); ?>"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <script>
             $(document).bind("mobileinit", function(){
@@ -8,11 +8,11 @@
             });
         </script>
         <script src="//ajax.googleapis.com/ajax/libs/jquerymobile/1.4.5/jquery.mobile.min.js"></script>
-        <script src="<?php printf('%s', ASSETS . '/js/browser_detect.js'); ?>"></script>
-        <script src="<?php printf('%s', ASSETS . '/js/main.js'); ?>"></script>
-        <script src="<?php printf('%s', ASSETS . '/js/nav.js'); ?>"></script>
-        <script src="<?php printf('%s', ASSETS . '/js/gallery.js'); ?>"></script>
-        <script src="<?php printf('%s', ASSETS . '/js/team.js'); ?>"></script>
+        <script src="<?php printf('%s', ASSETS . '/js/browser_detect.min.js'); ?>"></script>
+        <script src="<?php printf('%s', ASSETS . '/js/main.min.js'); ?>"></script>
+        <script src="<?php printf('%s', ASSETS . '/js/nav.min.js'); ?>"></script>
+        <script src="<?php printf('%s', ASSETS . '/js/gallery.min.js'); ?>"></script>
+        <script src="<?php printf('%s', ASSETS . '/js/team.min.js'); ?>"></script>
         <?php // Load /all/ the scripts! ?>
     </body>
 </html>
