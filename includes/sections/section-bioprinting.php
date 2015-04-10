56b3616a1737a5ee18cb65b95638ac8c952b0c84
Mark <mark@bhalash.com>
2015-04-11 00:02:13 +0100

    Renamed sections and removed text per directions

--- includes/sections/section-bioprinting.php
+++ includes/sections/section-bioprinting.php
@@ -4,17 +4,16 @@
         <div class="bioprint-panes">
             <div class="pane">
                 <div class="pane-image pane-conceive"></div>
-                <h5>Conceive</h5>
+                <h5>Create</h5>
             </div>
             <div class="pane">
                 <div class="pane-image pane-print"></div>
-                <h5>Create</h5>
+                <h5>Print</h5>
             </div>
             <div class="pane">
                 <div class="pane-image pane-iterate"></div>
                 <h5>Iterate</h5>
             </div>
         </div>
-        <p>The prototyping cycle is integral to bioprinting: dream it, draw it, print it and repeat. Prototyping is also a time- and money-intensive process, with prints taking upward of a day to create.</p> 
     </div>
 </section>
