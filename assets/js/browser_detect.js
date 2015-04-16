045380c19aeb530ef623ca2e8ee366baf3d05a4f
Mark <mark@bhalash.com>
2015-04-16 03:12:28 +0100

    Added more detection rules

--- assets/js/browser_detect.js
+++ assets/js/browser_detect.js
@@ -1,9 +1,14 @@
 (function() {
     'use strict';
-    /*
+    /**
      * Ghetto Browser Detection
      * ------------------------
      * Now with 100% less jQuery! \o/
+     * Please don't use this to disable features on your page; my own goal for 
+     * it stems from needing to tweak layout because of browser quirks.
+     * 
+     * This is not a canonical list of every browser and OS; I tend to add to 
+     * this as required.
      */
 
     var agent = navigator.userAgent.toLowerCase();
@@ -13,6 +18,13 @@
         // Any handheld device.
         html.classList.add('mobile');
     }
+
+    /**
+     * iOS and iOS Versions
+     * --------------------
+     * There are quirks related to flexbox and the vh unit in older versions of
+     * iOS.
+     */
 
     if (!!agent.match(/i(pad|phone|pod).+(version\/6\.\d+ mobile)/i)) {
         // iOS 6
@@ -29,19 +41,39 @@
         html.classList.add('ios-8');
     }
 
-    if (!!agent.match(/ipad/i)) {
-        // iPad
-        html.classList.add('ipad');
+    if (!!agent.match(/i(pad|phone|pod)/i)) {
+        // iOS
+        html.classList.add('ios');
     }
+
+    /**
+     * Desktop Operating Systems
+     * -------------------------
+     */
+
+    if (/windows\snt/.test(agent)) {
+        // Microsoft Windows
+        html.classList.add('windows');
+    }
+
+    if (/linux/.test(agent)) {
+        // Linux 
+        html.classList.add('linux');
+    }
+
+    if (/os\sx/.test(agent) || /macintosh/.test(agent)) {
+        // OS X
+        html.classList.add('osx');
+    }
+
+    /**
+     * Browsers
+     * --------
+     */
 
     if (/webkit/.test(agent)) {
-        // Webkit 
+        // General Webkit 
         html.classList.add('webkit');
-    }
-
-    if (/trident/.test(agent)) {
-        // Internet Explorer
-        html.classList.add('internet-explorer');
     }
 
     if (/chrome/.test(agent)) {
@@ -52,5 +84,15 @@
     if (/firefox/.test(agent)) {
         // Mozilla Firefox
         html.classList.add('firefox');
+    }
+
+    if (/msie/.test(agent) || /trident/.test(agent)) {
+        // Microsoft Internet Explorer
+        html.classList.add('ie');
+    }
+
+    if (/safari/.test(agent)) {
+        // Apple Safari
+        html.classList.add('safari');
     }
 })();
