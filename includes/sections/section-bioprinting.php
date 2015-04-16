77ebb2f4a2b512271e3413e6f03250575c736b7c
Mark <mark@bhalash.com>
2015-04-16 03:18:52 +0100

    Added code formerly of header

--- includes/sections/section-bioprinting.php
+++ includes/sections/section-bioprinting.php
@@ -1,18 +1,17 @@
 <section id="bioprint">
     <div class="content">
-        <h3 class="sec-title">The Bioprinting Process</h3>
+        <h3>Introducing the <?php printf(PRODUCT_NAME); ?> Bioprinter:</h3>
+        <p><?php printf(PRODUCT_NAME); ?>is the next generation of bioprinting technology: a lower-cost tissue engineering platform with superior print resolution. 
+        Find out more about how <?php printf(PRODUCT_NAME); ?> can help you on <a title="The <?php printf(COMPANY_NAME); ?>" rel="external" href="<?php printf(COMPANY_BLOG); ?>">our blog</a> or reach out to us <a title="<?php printf(COMPANY_NAME); ?> on Twitter" href="//twitter.com/ouro_botics" rel="external nofollow">on Twitter</a>.</p>
         <div class="bioprint-panes">
             <div class="pane">
-                <div class="pane-image pane-conceive"></div>
-                <h5>Create</h5>
+                <div class="pane-image pane-design"></div>
             </div>
             <div class="pane">
                 <div class="pane-image pane-print"></div>
-                <h5>Print</h5>
             </div>
             <div class="pane">
                 <div class="pane-image pane-iterate"></div>
-                <h5>Iterate</h5>
             </div>
         </div>
     </div>
